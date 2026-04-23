48e647e7ddbb263475078bbe9bb1e3af6a287cc1
Long <vuminhl@sheridancollege.ca>
2026-04-22 22:09:15 -0400

    Add counter. Done with the left mask

--- CarCounter.py
+++ CarCounter.py
@@ -7,13 +7,14 @@ from sort import *
 # more weight more accurate and detailed but also slower
 model = YOLO('yolov8l.pt')
 model_names = model.names
-
+# store the id that cross the line
+totalCount = [];
 
 cap = cv2.VideoCapture("./video/cars1.mp4")
 # the mask is created by using canva where you cover all unnessary details using
 # a black box
 left_mask = cv2.imread("./mask/left_mask.png")
-limits = [220, 230, 300, 230]
+limits = [180, 230, 310, 230]
 
 # tracking
 # max_age: maximum numnber of frames the we still recognize it
@@ -49,9 +50,7 @@ while True:
             # each model might have varying class name, thus we will use model.names to get all the name
             currentClass = model_names[int(box.cls[0])]
             
-            cv2.rectangle(imgRegion,(x1,y1), (x2,y2),(13, 13, 13), 1)
-        # Write the class name and confidence percentage
-            cv2.putText(imgRegion, f"{currentClass}: {conf}%",(x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (51, 255, 51), 1)
+   
             
             
             if (currentClass == "car" or currentClass == "truck" or currentClass == "bus") and conf > 50:
@@ -69,12 +68,18 @@ while True:
         print(trackerResult)
         cv2.rectangle(img,(x1,y1), (x2,y2),(13, 13, 13), 1)
         # Write the class name and confidence percentage
-        cv2.putText(img, f"{currentClass}: {id}",(x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (51, 255, 51), 1)
+        cv2.putText(img, f"{id}",(x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (51, 255, 51), 1)
+        center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
+        cv2.circle(img, (center_x, center_y), 3, (255,0,255), 1)
+        if limits[0] - 5 < center_x < limits[2] + 5 and  limits[1] - 5 < center_y < limits[3] + 5:
+            if(totalCount.count(id) == 0):
+                totalCount.append(id)
         
+    cv2.putText(img, f"Count: {len(totalCount)}",(50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (1, 50, 32), 1)
         
     cv2.imshow("Image", img)
     cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
 
 
 # show will display in seperate window, we are having trouble with cv2.waitKey() since it doesn't work
